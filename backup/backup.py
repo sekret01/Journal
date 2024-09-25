2b50d6078b5da9a1d45843b999abf0b1c2ac0008
sekret01 <152257881+sekret01@users.noreply.github.com>
2024-09-25 11:45:12 +0300

    Update backup.py
    clear backup`s path

--- backup/backup.py
+++ backup/backup.py
@@ -1,11 +1,11 @@
 import shutil
 
 # пути для запуска из backup.py
-SOURCE = 'P:/micro_DB/Journal_data/data.json'
+SOURCE = ... # path to write backup
 FILE_PATH = '../journal_pack/data/data.json'
 
 # пути для запуска из main.py
-OTHER_SOURCE = 'P:/micro_DB/Journal_data/data.json'
+OTHER_SOURCE = ... # path to read backup
 OTHER_FILE_PATH = 'journal_pack/data/data.json'
 
 
